13a66913115dc483a33674af1acda14015616cb4
Aleksandar Gekov <alexander.gekov@cytosmart.com>
2022-03-08 23:25:05 +0200

    added login page and provider keys

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { theme } from "@chakra-ui/pro-theme";
+import "@fontsource/inter/variable.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const myTheme = extendTheme(
+    {
+      colors: { ...theme.colors, brand: theme.colors.purple },
+    },
+    theme
+  );
+  return (
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <ChakraProvider theme={myTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import TwitterProvider from "next-auth/providers/twitter";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+      version: "2.0",
+    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET,
+    // }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
